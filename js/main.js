ce5e837b941d94a87d71b539d0ce2e26ac5fee39
DevNathJoydeep <devnathjoydeep@gmail.com>
2018-09-27 03:52:01 +0530

    thirdCommit

--- js/main.js
+++ js/main.js
@@ -49,6 +49,10 @@ $(document).ready(function() {
       $("#myResume").css("background", "#075144");
       $(".workDesc").css("background", "rgba(7,81,68,0.85)");
       $(".workButton").css("color", "#39c493");
+      $(".close").css("color", "#51d3a6");
+      $(".formHeading").css("color", "#51d3a6");
+      $(".labels").css("color", "#51d3a6");
+      $(".formBtn").css("background", "#51d3a6");
       setTimeout(function() {
         $("meta[name='theme-color']").attr("content", "#5db7d4");
       }, 500);
@@ -62,6 +66,10 @@ $(document).ready(function() {
       $("#myResume").css("background", "#3b2f61");
       $(".workDesc").css("background", "rgba(106, 54, 255, 0.85)");
       $(".workButton").css("color", "#6a36ff");
+      $(".close").css("color", "#6a36ff");
+      $(".formHeading").css("color", "#6a36ff");
+      $(".labels").css("color", "#6a36ff");
+      $(".formBtn").css("background", "#6a36ff");
       setTimeout(function() {
         $("meta[name='theme-color']").attr("content", "#4080ff");
       }, 500);
@@ -72,4 +80,30 @@ $(document).ready(function() {
   $("#changeTheme").on("click", function() {
     toggleTheme();
   });
+
+  //----------------------------
+
+  $("#formContainer").fadeOut("fast");
+
+  $("#letsDoItBtn").on("click", function() {
+    $("#getInTouchForm").css("width", "100%");
+    $("#getInTouchForm").css("height", "100%");
+    $("#getInTouchForm").css("top", "0");
+    $("#getInTouchForm").css("left", "0");
+    setTimeout(function() {
+      $("#formContainer").fadeIn("slow");
+    }, 500);
+  });
+
+  $("#closeForm").on("click", function() {
+    $("#formContainer").fadeOut("slow");
+    setTimeout(function() {
+      $("#getInTouchForm").css("width", "0");
+      $("#getInTouchForm").css("height", "0");
+      $("#getInTouchForm").css("top", "50%");
+      $("#getInTouchForm").css("left", "50%");
+    }, 500);
+  });
+
+  //----------------------------
 });
